e3aadf64d7dbca784c4164aceffc0e6200b93076
AswanthGPillai <aswanthgpillai@gmail.com>
2026-06-30 15:08:29 +0530

    edit

--- Administrator/urls.py
+++ Administrator/urls.py
@@ -7,4 +7,5 @@ app_name="Administrator"
 urlpatterns = [
     path('AdminRegistration/',views.AdminRegistration,name="AdminRegistration"),
     path('DeleteAdmin/<int:did>',views.DeleteAdmin,name="DeleteAdmin"),
+    path('EditAdmin/<int:eid>',views.EditAdmin,name="EditAdmin"),
 ]
--- Administrator/views.py
+++ Administrator/views.py
@@ -22,3 +22,21 @@ def AdminRegistration(request):
 def DeleteAdmin(request,did):
     tbl_admin.objects.get(id=did).delete()
     return redirect('Administrator:AdminRegistration')
+    # return render(request,'Administrator/AdminRegistration.html')
+
+def EditAdmin(request,eid):
+    editdata=tbl_admin.objects.get(id=eid)
+    if request.method=='POST':
+        name=request.POST.get('txt_name')
+        email=request.POST.get('txt_email')
+        contact=request.POST.get('txt_contact')
+        password=request.POST.get('txt_password')
+
+        editdata.admin_name=name
+        editdata.admin_email=email
+        editdata.admin_contact=contact
+        editdata.admin_password=password
+        editdata.save()
+        return redirect('Administrator:AdminRegistration')
+    else:
+        return render(request,'Administrator/AdminRegistration.html',{'editdata':editdata})
