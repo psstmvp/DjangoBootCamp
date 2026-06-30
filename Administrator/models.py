94d1005274aedbf17238cb0cea3d081fdddb03b2
AswanthGPillai <aswanthgpillai@gmail.com>
2026-06-30 09:39:29 +0530

    updated

--- Administrator/models.py
+++ Administrator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class tbl_admin(models.Model):
+    admin_name = models.CharField(max_length=100)
+    admin_email = models.EmailField(unique=True)
+    admin_password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
