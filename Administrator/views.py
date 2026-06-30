1744ab2b2e88d08ad5b32973b18ae9ad0061e568
AswanthGPillai <aswanthgpillai@gmail.com>
2026-06-30 11:40:33 +0530

    select

--- Administrator/views.py
+++ Administrator/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 from Administrator.models import *
 # Create your views here.
 def AdminRegistration(request):
+    ## Select Query 
+    admindata = tbl_admin.objects.all()
     if request.method=='POST':
         name=request.POST.get('txt_name')
         email=request.POST.get('txt_email')
         contact=request.POST.get('txt_contact')
         password=request.POST.get('txt_password')
+        ## Insert Query..
         tbl_admin.objects.create(admin_name=name,
                                  admin_email=email,
                                  admin_contact=contact,
                                  admin_password=password)
-        return render(request,'Administrator/AdminRegistration.html')
+        return render(request,'Administrator/AdminRegistration.html',{'admindata':admindata})
     else:
-        return render(request,'Administrator/AdminRegistration.html')
+        return render(request,'Administrator/AdminRegistration.html',{'admindata':admindata})
 
